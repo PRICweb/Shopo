209fe6f5471b02f31befe2ebaf10a68631f020cc
PRICweb <pric.administracion@gmail.com>
2026-09-07 23:00:36 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-const CACHE_VERSION = 'shopo-pwa-v14';
+const CACHE_VERSION = 'shopo-pwa-v15';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const APP_SCOPE = '/Shopo/';
 const PRECACHE = [
